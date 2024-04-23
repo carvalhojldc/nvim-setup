fe14e664e2a80b88eac41b846f3d4508fb2d797a
JCarvalho <carvalhojldc@gmail.com>
2024-04-23 19:37:44 -0300

    Using KICKSTART.NVIM to genrate new setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,103 +1,227 @@
-local vim = vim
-local Plug = vim.fn['plug#']
+-- ----------------------------------------------------------------------------
+-- Based on KICKSTART.NVIM
+--
 
-vim.cmd('source ~/.config/nvim/config.vim')
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.call('plug#begin')
-    ---------------------------------------
-    -- Appearance
-    Plug('nvim-tree/nvim-web-devicons')
-    Plug('akinsho/bufferline.nvim', { ['tag'] = '*' })
-    Plug('nvim-lualine/lualine.nvim')
+vim.g.noswap = true
 
-    -- Themes
-    Plug('sainnhe/sonokai')
-    Plug('sainnhe/gruvbox-material')
-    Plug('folke/tokyonight.nvim')
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
-    ---------------------------------------
-    -- Completion / linters / formatters
-    -- Plug('thaerkh/vim-indentguides')
-    --Plug 'sheerun/vim-polyglot'
-    Plug('sindrets/diffview.nvim')
-    Plug('lewis6991/gitsigns.nvim')
-    Plug('numToStr/Comment.nvim')
-    Plug('mg979/vim-visual-multi')
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 
-    Plug('neovim/nvim-lspconfig')
-    Plug('hrsh7th/cmp-nvim-lsp')
-    Plug('hrsh7th/nvim-cmp')
-    Plug('hrsh7th/cmp-buffer')
-    Plug('saadparwaiz1/cmp_luasnip')  -- Snippets source for nvim-cmp
-    Plug('L3MON4D3/LuaSnip')          -- Snippets plugin
-    Plug('rafamadriz/friendly-snippets')
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-    Plug('nvim-lua/plenary.nvim')
-    Plug('nvim-telescope/telescope.nvim')
-    Plug('nvim-tree/nvim-tree.lua')
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
 
-vim.call('plug#end')
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = true
 
-require('theme')
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Configure tab and spaces
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+-- Show color column
+vim.opt.cc = '100'
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
 
 -- disable netrw at the very start
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
+
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Split window
+vim.keymap.set("n", "tv", "<cmd>vsplit<cr>", { desc = "split vertical" })
+vim.keymap.set("n", "th", "<cmd>split<cr>", { desc = "split horizontal" })
+
+-- Quit and save
+vim.keymap.set("n", "tt", "<cmd>q<cr>", { desc = "quit" })
+vim.keymap.set("n", "<c-s>", "<cmd>w<cr>", { desc = "save" })
+
+-- Navigate between buffers
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-x>", "<cmd>bwipe<cr>", { desc = "Close buffer" })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+
+    {
+        'folke/which-key.nvim',
+        version = 'v1.6.0',
+        event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+        config = function() -- This is the function that runs, AFTER loading
+            require('which-key').setup()
+
+            -- Document existing key chains
+            require('which-key').register {
+                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+                ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+                ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+            }
+            -- visual mode
+            require('which-key').register({
+                ['<leader>h'] = { 'Git [H]unk' },
+            }, { mode = 'v' })
+        end,
     },
-    view = {
-        width = 30,
+
+    require 'plugins.telescope',
+    require 'plugins.gitsigns',
+    require 'plugins.neo-tree',
+    require 'plugins.treesitter',
+    require 'plugins.theme',
+    require 'plugins.lsp',
+
+    -- "gc" to comment visual regions/lines
+    { 
+        'numToStr/Comment.nvim',
+        version = 'v0.8.0',
+        opts = {},
     },
-    renderer = {
-        group_empty = true,
+
+    {
+        'akinsho/bufferline.nvim',
+        version = 'v4.5.2',
+        opts = {},
     },
-    filters = {
+
+}, {
+    ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+            cmd = '⌘',
+            config = '🛠',
+            event = '📅',
+            ft = '📂',
+            init = '⚙',
+            keys = '🗝',
+            plugin = '🔌',
+            runtime = '💻',
+            require = '🌙',
+            source = '📄',
+            start = '🚀',
+            task = '📌',
+            lazy = '💤 ',
+        },
     },
 })
 
-require('lualine').setup{
-  options = {
-    --theme = 'gruvbox-material',
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '', right = '' },
-  }
-}
-
-require("bufferline").setup{}
-
-require('gitsigns').setup {
-  signs = {
-    add          = { text = '+' },
-    change       = { text = 'u' },
-    delete       = { text = '_' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
-  }, 
-}
-
-require('diffview').setup{}
-
-require('Comment').setup{}
-
-vim.keymap.set('n', '<leader>gd',
-function()
-    if next(require('diffview.lib').views) == nil then
-        vim.cmd('DiffviewOpen')
-    else
-        vim.cmd('DiffviewClose')
-    end
-end)
-
-vim.keymap.set('n', '<leader>gh',
-function()
-    if next(require('diffview.lib').views) == nil then
-        vim.cmd('DiffviewFileHistory')
-    else
-        vim.cmd('DiffviewClose')
-    end
-end)
-
-require('lsp_config')
